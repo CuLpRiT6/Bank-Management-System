f8e3ad7dff7f52139d8208e31af28e5c4de64fb5
LakshayBansal06 <lakshaybansal123321@gmail.com>
2025-03-26 00:46:22 +0530

    added

---
+++ src/bank/management/system/Signup2.java
@@ -0,0 +1,37 @@
+package bank.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Signup2 extends JFrame  {
+
+
+
+    Signup2(){
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/bank.png"));
+        Image i2 = i1.getImage().getScaledInstance(100,100, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(150,5,100,100);
+        add(image);
+
+
+
+
+
+
+
+        setLayout(null);
+        setSize(850,750);
+        setLocation(450,80);
+        getContentPane().setBackground(new Color(252,208,76));
+        setVisible(true);
+    }
+
+    public static void main(String[] args){
+        new Signup2();
+
+    }
+
+}
